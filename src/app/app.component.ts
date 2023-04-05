bef213dd9ce8f70e7619404fdac397ce8d160096
Cristian Istudor <cristianistudor27@gmail.com>
2023-04-05 19:34:15 +0300

    LandingPage[WIP]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -8,8 +8,20 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'market-place-fe';
-  
+
+  isDarkMode: boolean = false;
+
+  @HostBinding('class.dark-theme')
+  get darkTheme() {
+    return this.isDarkMode;
+  }
+
+  toggleThemeMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
   constructor (
     private matIconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer
